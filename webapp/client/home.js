a973b4cb8efe7bb5006cf5d9175f71aead4f8d3e
Prasenjit Dey <pdey@instart.co>
2016-05-24 17:24:01 +0530

    Storing test result with browser session.

--- webapp/client/home.js
+++ webapp/client/home.js
@@ -1,5 +1,7 @@
 // Js helpers for the main AB testing templates.
 
+var currentPair = null;
+
 function getRandomVideoPair() {
   var pairs = VideoPairs.find().fetch();
   var randomPair = _.chain(pairs).shuffle().sample().value();
@@ -12,8 +14,16 @@ function getVideoURL(wptId) {
   return fs.url();
 };
 
+function saveResult(comp) {
+  console.log(currentPair, comp);
+  TestResults.insert({
+    pairId: currentPair._id,
+    session: Session.get('userSessionKey'),
+    result: comp
+  });
+}
+
 function preloadGifs(url1, url2) {
-  console.log("Preloading:", url1, url2);
   // Remove existing gif images.
   if($('#gifVideo1')) {
     $('#gifVideo1').attr('src', '');
@@ -36,10 +46,8 @@ function preloadGifs(url1, url2) {
   secondGif.onload = function() {syncGifLoad(secondGif);};
 
   function syncGifLoad(imgElem) {
-    console.log("gif load callback");
     numLoaded++;
     if(numLoaded == 2) {
-      console.log("Appending");
       $(firstGif).attr('id', 'gifVideo1');
       $(secondGif).attr('id', 'gifVideo2');
       $('.first-gif').append($(firstGif));
@@ -51,30 +59,63 @@ function preloadGifs(url1, url2) {
 
 Template.abTest.events({
   'click .show-next': function(e, t) {
+    t.$('.btn-decision').prop('disabled', false);
+    t.$('.show-next').prop('disabled', true);
     e.preventDefault();
     // remove current gifs
-    $('.gif').attr('src', '');
-    var newPair = getRandomVideoPair();
-    $('#gifVideo1').attr('src', getVideoURL(newPair.wptId_1));
-    $('#gifVideo2').attr('src', getVideoURL(newPair.wptId_2));
+    currentPair = getRandomVideoPair();
+    preloadGifs(
+      getVideoURL(currentPair.wptId_1),
+      getVideoURL(currentPair.wptId_2)
+    );
   },
 
   'click .replay-btn': function(e, t) {
+    t.$('.btn-decision').prop('disabled', false);
+    t.$('.show-next').prop('disabled', true);
     e.preventDefault();
-    console.log("Replay");
     var first = $('#gifVideo1').attr('src');
     var second = $('#gifVideo2').attr('src');
 
     // Reset
     preloadGifs(first, second);
+  },
+
+  // Results
+  'click .mid-btn': function(e, t) {
+    t.$('.btn-decision').prop('disabled', true);
+    t.$('.show-next').prop('disabled', false);
+    e.preventDefault();
+    saveResult(0);
+  },
+
+  'click .left-btn': function(e, t) {
+    t.$('.btn-decision').prop('disabled', true);
+    t.$('.show-next').prop('disabled', false);
+    e.preventDefault();
+    saveResult(1);
+  },
+
+  'click .right-btn': function(e, t) {
+    t.$('.btn-decision').prop('disabled', true);
+    t.$('.show-next').prop('disabled', false);
+    e.preventDefault();
+    saveResult(2);
   }
+
 });
 
-Template.gifView.onCreated(function(){
-  console.log('rendered');
+Template.abTest.onRendered(function(){
+  $('.show-next').prop('disabled', true);
+  // Generate a random hash for this user and store in session
+  Session.set('userSessionKey', Random.id());
+});
+
+Template.gifView.onRendered(function(){
   Meteor.setTimeout(function(){
-    var pair = getRandomVideoPair();
-    console.log(pair);
-    preloadGifs(getVideoURL(pair.wptId_1), getVideoURL(pair.wptId_2));
+    currentPair = getRandomVideoPair();
+    preloadGifs(
+      getVideoURL(currentPair.wptId_1),
+      getVideoURL(currentPair.wptId_2));
   }, 2000);
 });
