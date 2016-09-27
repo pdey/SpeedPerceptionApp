2cd1d31ef62228f585611a271782e016dcd99e27
Prasenjit Dey <pdey@instart.co>
2016-09-27 11:51:46 +0530

    Experiment expiry page.

--- webapp/client/routes.js
+++ webapp/client/routes.js
@@ -9,6 +9,10 @@ Router.configure({
 // Home route
 Router.route('/', {
 	action: function() {
+		if (Meteor.settings.public.expt_expired) {
+			this.render('experiment_expired');
+			return;
+		}
 		this.render('about');
 	}       
 });
@@ -19,13 +23,18 @@ Router.route('/challenge', {
 
 	waitOn: function() {
 		return [
-			Meteor.subscribe('videoPairs'),
-			Meteor.subscribe('videos'),
-			Meteor.subscribe('videoUploads')
+		Meteor.subscribe('videoPairs'),
+		Meteor.subscribe('videos'),
+		Meteor.subscribe('videoUploads')
 		];
 	},
 
 	action: function() {
+		if (Meteor.settings.public.expt_expired) {
+			this.render('experiment_expired');
+			return;
+		}
+
 		this.render('home');
 	}
 });
@@ -43,52 +52,52 @@ Router.route('/people', {
 		return {
 			teamMembers:
 			[
-				{
-					name: 'Clark Gao',
-					website: 'https://www.linkedin.com/in/clark-g-84a32530',
-					photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAARcAAAAJDI0ZWZiMjZlLTFlYjAtNDQzYS1iYTZjLTYxN2U5NjNiZTk0Yw.jpg',
-					designation: 'Data Science Engineer',
-					company_name: 'Instart Logic',
-					company_website: 'https://www.instartlogic.com/'
-				},
+			{
+				name: 'Clark Gao',
+				website: 'https://www.linkedin.com/in/clark-g-84a32530',
+				photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAARcAAAAJDI0ZWZiMjZlLTFlYjAtNDQzYS1iYTZjLTYxN2U5NjNiZTk0Yw.jpg',
+				designation: 'Data Science Engineer',
+				company_name: 'Instart Logic',
+				company_website: 'https://www.instartlogic.com/'
+			},
 
-				{
-					name: 'Parvez Ahammad, PhD',
-					website: 'https://www.linkedin.com/in/parvezahammad',
-					photo: 'https://media.licdn.com/media/p/3/000/206/3ec/070cd62.jpg',
-					designation: 'Head of Data Science & Machine Learning',
-					company_name: 'Instart Logic',
-					company_website: 'https://www.instartlogic.com/'
-				},
+			{
+				name: 'Parvez Ahammad, PhD',
+				website: 'https://www.linkedin.com/in/parvezahammad',
+				photo: 'https://media.licdn.com/media/p/3/000/206/3ec/070cd62.jpg',
+				designation: 'Head of Data Science & Machine Learning',
+				company_name: 'Instart Logic',
+				company_website: 'https://www.instartlogic.com/'
+			},
 
-				{
-					name: 'Prasenjit Dey',
-					website: 'https://www.linkedin.com/in/prasenjitdey',
-					photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0b6/3f9/31c2ca6.jpg',
-					designation: 'Software Engineer',
-					company_name: 'Instart Logic',
-					company_website: 'https://www.instartlogic.com/'
-				}
+			{
+				name: 'Prasenjit Dey',
+				website: 'https://www.linkedin.com/in/prasenjitdey',
+				photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0b6/3f9/31c2ca6.jpg',
+				designation: 'Software Engineer',
+				company_name: 'Instart Logic',
+				company_website: 'https://www.instartlogic.com/'
+			}
 			],
 			coMembers:
 			[
-				{
-					name: 'Estelle Weyl',
-					website: 'https://www.linkedin.com/in/estellevw',
-					photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/005/0b1/383/3eed648.jpg',
-					designation: 'Open Web Evangelist',
-					company_name: 'Instart Logic',
-					company_website: 'https://www.instartlogic.com/'
-				},
-				
-				{
-					name: 'Patrick Meenan',
-					website: 'https://www.linkedin.com/in/patrickmeenan',
-					photo: 'https://media.licdn.com/media/AAEAAQAAAAAAAAOtAAAAJDhlN2FhZTczLTE3NWYtNDQyYi05NGNmLTBiNjBmODM1NDM5Mw.jpg',
-					designation: 'Staff Engineer at Google',
-					company_name: 'WebPagetest LLC',
-					company_website: 'http://www.webpagetest.org/'
-				}
+			{
+				name: 'Estelle Weyl',
+				website: 'https://www.linkedin.com/in/estellevw',
+				photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/005/0b1/383/3eed648.jpg',
+				designation: 'Open Web Evangelist',
+				company_name: 'Instart Logic',
+				company_website: 'https://www.instartlogic.com/'
+			},
+
+			{
+				name: 'Patrick Meenan',
+				website: 'https://www.linkedin.com/in/patrickmeenan',
+				photo: 'https://media.licdn.com/media/AAEAAQAAAAAAAAOtAAAAJDhlN2FhZTczLTE3NWYtNDQyYi05NGNmLTBiNjBmODM1NDM5Mw.jpg',
+				designation: 'Staff Engineer at Google',
+				company_name: 'WebPagetest LLC',
+				company_website: 'http://www.webpagetest.org/'
+			}
 			]
 		};
 	},
@@ -110,11 +119,11 @@ Router.route('/admin', {
 
 	waitOn: function() {
 		return [
-			Meteor.subscribe('datasets'),
-			Meteor.subscribe('videos'),
-			Meteor.subscribe('videoPairs'),
-			Meteor.subscribe('videoUploads'),
-			Meteor.subscribe('testResults')
+		Meteor.subscribe('datasets'),
+		Meteor.subscribe('videos'),
+		Meteor.subscribe('videoPairs'),
+		Meteor.subscribe('videoUploads'),
+		Meteor.subscribe('testResults')
 		];
 	},
 
